86988f46d2c5a8c8f5e68baface78d1c231799e1
chenry22 <112424609+chenry22@users.noreply.github.com>
2025-11-15 16:24:46 -0600

    add card rarities

--- src/app/pack-page/pack-page.ts
+++ src/app/pack-page/pack-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
@@ -7,94 +7,137 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './pack-page.html',
   styleUrl: './pack-page.css',
 })
-export class PackPage {
-  public pages: any;
+export class PackPage implements OnInit {
+  public pages: any[] = [];
 
-  async getRandomWikiPages(count=5) {
-    var url = "https://en.wikipedia.org/w/api.php"; 
+  ngOnInit() {
+    this.openPack();
+  }
 
+  async openPack() {
+    this.pages = [];
+    for(var i = 0; i < 6; i++) {
+      var rarity;
+      var chance = Math.random();
+      if (chance < 0.38) {
+        rarity = "common";
+      } else if (chance < 0.66) {
+        rarity = "uncommon"
+      } else if (chance < 0.84) {
+        rarity = "rare"
+      } else if (chance < 0.94) {
+        rarity = "epic"
+      } else {
+        rarity = "legendary"
+      }
+
+      var page = await this.getRandomWikiPages(1, rarity);
+      this.pages.push(page[0]);
+    }
+  }
+
+  // wikipedia api stuff
+  async getRandomWikiPages(count=5, rarity="common") {
+    var minsize, maxsize;
+    switch(rarity) {
+      case "common":
+        minsize = "500"
+        maxsize = "1000"
+        break;
+      case "uncommon":
+        minsize = "1000"
+        maxsize = "5000"
+        break;
+      case "rare":
+        minsize = "5000"
+        maxsize = "20000"
+        break;
+      case "epic":
+        minsize = "20000"
+        maxsize = "100000"
+        break;
+      case "legendary":
+        minsize = "100000"
+        maxsize= "10000000"
+        break;
+      default:
+        return []; // invalid, don't load
+    }
+
+    // get random articles from api 
     var params = {
       action: "query",
       format: "json",
       list: "random",
       rnlimit: String(count),
       rnnamespace: "0", // main, so only real articles
-      minsize: "500", // min number of bytes in each article
+      rnminsize: minsize, // min number of bytes in each article
+      rnmaxsize: maxsize
     };
-
-    url = url + "?origin=*";
-    Object.entries(params).forEach(
-      ([key, value]) => url += "&" + key + "=" + value
-    );
+    var url = "https://en.wikipedia.org/w/api.php?origin=*&" + new URLSearchParams(params).toString();;
 
     const response = await (await fetch(url)).json();
-    this.pages = response.query.random;
-    for (var i in this.pages) {
+    var pages = response.query.random;
+    for (var i in pages) {
+      pages[i].class = "card " + rarity
       // get data for page
       const params = {
         action: "opensearch",
-        search: this.pages[i].title,
+        search: pages[i].title,
         limit: "1",
         namespace: "0",
         format: "json",
       };
       url = "https://en.wikipedia.org/w/api.php?origin=*&" + new URLSearchParams(params).toString();
       const r = await (await fetch(url)).json();
-      this.pages[i].desc = r[2][0];
-      this.pages[i].link = String(r[3][0]);
+      pages[i].link = String(r[3][0]);
 
 
       // get page image
       const params3 = {
         action: 'query',
-        titles: this.pages[i].title,
+        titles: pages[i].title,
         prop: 'pageimages',
+        pilicense: 'any',
         pithumbsize: '400',
         format: 'json',
         formatversion: '2'
       }
       url = "https://en.wikipedia.org/w/api.php?origin=*&" + new URLSearchParams(params3).toString()
-
       var rev = await (await fetch(url)).json();
-      console.log(rev);
+      if (rev.query.pages[0] && rev.query.pages[0].thumbnail) {
+        pages[i].thumbnail = rev.query.pages[0].thumbnail.source
+      } else {
+        pages[i].thumbnail = '';
+      }
 
 
-      // get page preview/description
+      // get page description/preview
       const params2 = {
-        action: "query",
-        format: "json",
-        prop: "revisions",
-        titles: this.pages[i].title,
-        formatversion: '2',
-        rvprop: 'content',
-        rvslots: '*'
-      };
-
+        action: 'query',
+        format: 'json',
+        prop: 'extracts',
+        exsentences: '4',
+        exlimit: '1',
+        titles: pages[i].title, // String(r[3][0]).split("/")[String(r[3][0]).split("/").length - 1],
+        explaintext: '1',
+        formatversion: '2'
+      }
       url = "https://en.wikipedia.org/w/api.php?origin=*&" + new URLSearchParams(params2).toString()
       var rev = await (await fetch(url)).json();
-      if (rev && rev.query && rev.query.pages && rev.query.pages[0].revisions) {
-        this.pages[i].desc = 
-          String(rev.query.pages[0].revisions[0].slots.main.content)
-            .substring(0, 500).split("}}");
-        var formatted = [];
-        for (var j in this.pages[i].desc) {
-          if (String(this.pages[i].desc[j]).includes("{{")) {
-            if (String(this.pages[i].desc[j]).includes("description|")) {
-              formatted.push(String(this.pages[i].desc[j]).substring(20));
-            } else {
-              formatted.push(String(this.pages[i].desc[j]).substring(2));
-            }
-          } else {
-            formatted.push(String(this.pages[i].desc[j]));
-          }
+      var maxLen = 300
+      if (rev.query.pages[0].extract) {
+        if (String(rev.query.pages[0].extract).length > maxLen) {
+          pages[i].desc = String(rev.query.pages[0].extract).substring(0, maxLen - 3) + "..."
+        } else {
+          pages[i].desc = String(rev.query.pages[0].extract)
         }
-        console.log(formatted);
-        this.pages[i].desc = formatted;
-        
       } else {
-        console.log(rev);
+        pages[i].desc = "[ No description ]"
       }
     }
+    console.log(pages);
+    return pages;
   }
 
   getPageLink(page: any) {
